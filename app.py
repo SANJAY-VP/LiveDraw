14b51cbf2740fa9207577ee4f8ce6d85b82915e5
sanjay vp <sanjayvp08@gmail.com>
2025-09-03 13:01:13 +0530

    deploy ready

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
